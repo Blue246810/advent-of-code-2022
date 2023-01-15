d6d9a6f4aa9ab51eb87eedb4873fbfaadc2a0f79
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-01-15 16:12:16 +1100

    Part 2 of Task 1

--- 1/calorie.py
+++ 1/calorie.py
@@ -17,3 +17,24 @@ for line in lines:
        count += line
 
 print(max(tally))
+
+# %% [markdown]
+# Part 2
+# %%
+with open("data.txt") as file:
+    lines = file.readlines()
+
+tally = []
+count = 0
+for line in lines:
+    if line.isspace():
+        tally += [count]
+        count = 0
+        continue
+    else:  
+       line = int(line)    
+       count += line
+
+tally.sort(reverse=True)
+print(tally)
+sum(tally[0:3])
