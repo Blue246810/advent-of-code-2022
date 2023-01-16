3d92adf08bc5adbadf1551185e0fa5dfc6375aaa
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-01-16 21:32:38 +1100

    Part 2 of Task 2

--- 2/rockpaper.py
+++ 2/rockpaper.py
@@ -50,3 +50,43 @@ plan['round_score'] = plan['strategy_score'] + plan['game_score']
 # %%
 plan.sum()
 
+# %% [markdown]
+# Part 2
+# %% [markdown]
+# X - lose 0 
+# Y - draw 3
+# Z - win 6
+# 
+# A/X - Rock - 1
+# B/Y - Paper - 2
+# C/Z - Scissors - 3
+
+# %%
+plan = pd.read_csv("../data/2/data.txt", names=["theirs", "round_end"], sep=" ")
+
+# %%
+def shape_to_choose(theirs, round_end):
+    if theirs == 'A' and round_end == 'X':
+        return 'Z'
+    elif theirs == 'A' and round_end == 'Y':
+        return 'X'
+    elif theirs == 'A' and round_end == 'Z':
+        return 'Y'
+    elif theirs == 'B' and round_end == 'X':
+        return 'X'
+    elif theirs == 'B' and round_end == 'Y':
+        return 'Y'
+    elif theirs == 'B' and round_end == 'Z':
+        return 'Z'
+    elif theirs == 'C' and round_end == 'X':
+        return 'Y'
+    elif theirs == 'C' and round_end == 'Y':
+        return 'Z'
+    elif theirs == 'C' and round_end == 'Z':
+        return 'X'
+
+plan['shape_to_choose'] = plan.apply(lambda row : shape_to_choose(row['theirs'], row['round_end']) , axis=1)
+plan['strategy_score'] = plan.apply(lambda row : map_letter_to_score(row['shape_to_choose']) , axis=1)
+plan['game_score'] = plan.apply(lambda row : game_score(row['theirs'], row['shape_to_choose']), axis=1)
+plan['round_score'] = plan['strategy_score'] + plan['game_score']
+plan.sum()
