5bed7a9925a1336ed495e732ab10eddc88ced07c
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-01-21 13:42:26 +1100

    Part 1 of Task 3 - create function to split strings in half

--- 3/rucksack.py
+++ 3/rucksack.py
@@ -1,2 +1,13 @@
 # %% [markdown]
 # Part 1
+# %%
+test = "abba"
+
+def split_string(text):
+    middle_index = int(len(text)/2)
+    first_half = text[0:middle_index]
+    second_half = text[middle_index:]
+    return (first_half, second_half)
+
+split_string(test)
+
