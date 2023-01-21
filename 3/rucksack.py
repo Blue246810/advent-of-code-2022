03bbaf70ca8aa9d04c496d88d60b308f88ee9a4d
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-01-21 14:41:01 +1100

    Part 1 of Task 3 - move all logic to single function

--- 3/rucksack.py
+++ 3/rucksack.py
@@ -1,36 +1,35 @@
 # %% [markdown]
 # Part 1
-# %%
-test = "abccde"
-
-def split_string(text):
-    middle_index = int(len(text)/2)
-    first_half = text[0:middle_index]
-    second_half = text[middle_index:]
-    return (first_half, second_half)
-
-both_parts = split_string(test)
-first = both_parts[0]
-second = both_parts[1]
 #%%
-def common_letters(first, second):
-    first_set = set(first)
-    second_set = set(second)
-    common = first_set.intersection(second_set)
-    return common
-# %%
-common_letters(first, second)
-#%%
-def letter_to_number(letter):
-    if letter.islower():
-        return ord(letter)-96
-    if letter.isupper():
-        return ord(letter)-38
+def row_to_score(row):
+    def split_string(text):
+        middle_index = int(len(text)/2)
+        first_half = text[0:middle_index]
+        second_half = text[middle_index:]
+        return (first_half, second_half)
 
-# %%
-number_list = []
-for letter in letters_in_common:
-    number = letter_to_number(letter)
-    number_list += [number]
+    both_parts = split_string(row)
+    first = both_parts[0]
+    second = both_parts[1]
 
-sum(number_list)
+    def common_letters(first, second):
+        first_set = set(first)
+        second_set = set(second)
+        common = first_set.intersection(second_set)
+        return common
+
+    letters_in_common = common_letters(first, second)
+
+    def letter_to_number(letter):
+        if letter.islower():
+            return ord(letter)-96
+        if letter.isupper():
+            return ord(letter)-38
+
+    number_list = []
+    for letter in letters_in_common:
+        number = letter_to_number(letter)
+        number_list += [number]
+
+    return sum(number_list)
+
