cc5f0b0521dae206e9d0c0c29160de367f58fecc
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-01-29 19:34:21 +1100

    Part 1 of Task 4 - splitcol funct

--- 4/cleanup.py
+++ 4/cleanup.py
@@ -7,3 +7,11 @@ data = pd.read_csv("../data/4/data.txt", names=["sections"], sep=" ")
 data = data["sections"].str.split(",", expand=True)
 data.columns = ["elf1", "elf2"]
 
+#%%
+def splitcol(column_name, name):
+    data = column_name.str.split("-", expand=True)
+    data.columns = [name + "low", name + "high"]
+    data[name + "low"] = data[name + "low"].astype(int)
+    data[name + "high"] = data[name + "high"].astype(int)
+    return data
+
