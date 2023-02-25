7e0d25257169193acdec31dfe0ec21404c4c7748
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-02-25 18:13:43 +1100

    Part 1 & 2 Task 6
    Part 1 - had to stop when 4 unique chars together
    Part 2 - had to stop when 14 unique chars together

--- 6/tuning.py
+++ 6/tuning.py
@@ -7,3 +7,10 @@ with open("../data/6/data.txt", 'r') as file:
 
 datastream
 
+#%% 
+for index in range(len(datastream)):
+    four_chars = datastream[index: index+14]
+    has_duplicates = len(set(four_chars)) != len(four_chars)
+    if not has_duplicates:
+        print(index+14)
+        break
