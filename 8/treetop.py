3b2119c6f4784bef82f8d095938acae5142981d6
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-03-18 15:47:46 +1100

    Part 1 of Task 8
    Read file into columns and rows

--- 8/treetop.py
+++ 8/treetop.py
@@ -9,3 +9,7 @@ DATAPATH = "../data/8/data.txt"
 grid = pd.read_csv(DATAPATH, header=None)
 grid_width = grid.shape[0]
 
+# %%
+# Read file into columns and rows
+trees = pd.read_fwf(DATAPATH, widths=[1] * grid_width, header=None)
+
