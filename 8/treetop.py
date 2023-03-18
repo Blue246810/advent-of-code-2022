32c5f468f3b1c515087b28e33b458f8d0b730fad
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-03-18 15:48:20 +1100

    Part 1 of Task 8
    Find trees visible from outside

--- 8/treetop.py
+++ 8/treetop.py
@@ -17,3 +17,30 @@ trees = pd.read_fwf(DATAPATH, widths=[1] * grid_width, header=None)
 # Calc edge of the tree grid
 grid_edge = (trees.shape[0]-1) * (trees.shape[1]-1)
 
+# %%
+# Find trees visible from outside
+import math 
+
+counter = 0
+inside_vis = []
+for row_number, row in trees.iterrows():
+    # print(row_number, row)
+    for col_number, entry in enumerate(row): 
+        # print(row_number, col_number)
+        above = trees[0: row_number][col_number]
+        max_above = above.max()
+        below = trees[row_number+1: ][col_number]
+        max_below = below.max()
+        left = trees.iloc[row_number][0: col_number]
+        max_left = left.max()
+        right = trees.iloc[row_number][col_number+1: ]
+        max_right = right.max()
+
+        if math.isnan(max_above) or math.isnan(max_below) or math.isnan(max_left) or math.isnan(max_right):
+            counter += 1
+            continue
+
+        if entry > max_above or entry > max_below or entry > max_left or entry > max_right:
+            counter += 1
+counter
+
