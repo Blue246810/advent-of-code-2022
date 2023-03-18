d9a197a3a5f5450835b492d0f00dbbbea237c8fd
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-03-18 15:48:03 +1100

    Part 1 of Task 8
    Calc edge of the tree grid

--- 8/treetop.py
+++ 8/treetop.py
@@ -13,3 +13,7 @@ grid_width = grid.shape[0]
 # Read file into columns and rows
 trees = pd.read_fwf(DATAPATH, widths=[1] * grid_width, header=None)
 
+# %%
+# Calc edge of the tree grid
+grid_edge = (trees.shape[0]-1) * (trees.shape[1]-1)
+
