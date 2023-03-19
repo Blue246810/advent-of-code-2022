8cea47f9733ed6c19e521d3786edb0236c9ce0ca
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-03-19 15:31:12 +1100

    Part 1 of Task 8
    Removing calc on grid edge & inside_vis variable that I did not end up using. Removed prints to clean up code visually.

--- 8/treetop.py
+++ 8/treetop.py
@@ -14,19 +14,12 @@ grid_width = grid.shape[0]
 trees = pd.read_fwf(DATAPATH, widths=[1] * grid_width, header=None)
 
 # %%
-# Calc edge of the tree grid
-grid_edge = (trees.shape[0]-1) * (trees.shape[1]-1)
-
-# %%
 # Find trees visible from outside
 import math 
 
 counter = 0
-inside_vis = []
 for row_number, row in trees.iterrows():
-    # print(row_number, row)
     for col_number, entry in enumerate(row): 
-        # print(row_number, col_number)
         above = trees[0: row_number][col_number]
         max_above = above.max()
         below = trees[row_number+1: ][col_number]
