dbb1907cd7bb7b3b8a06dc585dd99872d7bf373c
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-03-21 22:16:27 +1100

    Part 1 of Task 9
    Create variables for movement of head, tail and tail positions has been in

--- 9/ropebridge.py
+++ 9/ropebridge.py
@@ -6,3 +6,10 @@
 with open("../data/9/data.txt") as file:
     lines = [line.rstrip() for line in file]
 
+# %%
+# create variables for movement of head, tail and tail positions has been in
+head: tuple = (0,0)
+tail: tuple = (0,0)
+tail_pos: list = []
+allowable_distance = 1
+
