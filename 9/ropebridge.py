12865889c36fd4ea2a4fcc2d43d3fd224fda28d5
Blue246810 <88653914+Blue246810@users.noreply.github.com>
2023-03-21 22:16:52 +1100

    Part 1 of Task 9
    Where head moves to

--- 9/ropebridge.py
+++ 9/ropebridge.py
@@ -13,3 +13,19 @@ tail: tuple = (0,0)
 tail_pos: list = []
 allowable_distance = 1
 
+#%%
+# where head moves to 
+for row in lines:
+    direction = row.split(" ")[0]
+    steps = int(row.split(" ")[1])
+
+    for _ in range(steps):
+        if direction == "R":
+            head = (head[0]+1, head[1])
+        elif direction == "L":
+            head = (head[0]-1, head[1])
+        elif direction == "U":
+            head = (head[0], head[1]+1)
+        else:
+            head = (head[0], head[1]-1)
+
